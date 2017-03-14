e0c877fd6f1003a0a43ab047378abfd55b8500df
Chris_Wallace <kawallis@icloud.com>
2017-03-14 13:25:20 -0700

    added stuff

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,16 +1,21 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate a package.json file
+// DONE! TODO: Initialize your project using NPM to create and populate a package.json file
 
-// TODO: Require the Express package that you installed via NPM, and instantiate the app
+// DONE! TODO: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
+const express = require('express');
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// TODO: Include all of the static resources as an argument to app.use()
+// DONE! TODO: Include all of the static resources as an argument to app.use()
+app.use(express.static('./public'));
 
 app.get('*', function(request, response) {
   // TODO: Using the response object, send the index.html file back to the user
+  console.log('working');
+  response.sendFile('/public/index.html', {root: '.'});
 });
 
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
