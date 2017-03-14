fee57f4fcc4bb8c6f0ebb293899f457589bac6f9
Yuval Allweil <themarquisdesheric@gmail.com>
2017-03-14 13:47:26 -0700

    Finished lab and stretch goals

--- starter-code/server.js
+++ starter-code/server.js
@@ -7,19 +7,28 @@
 const express = require('express');
 const app = express();
 
+//if a port has been explicitly declared, use it. If not, default PORT to 3000
 const PORT = process.env.PORT || 3000;
 
 // DONE! TODO: Include all of the static resources as an argument to app.use()
 app.use(express.static('./public'));
 
-app.get('*', function(request, response) {
-  // TODO: Using the response object, send the index.html file back to the user
-  console.log('working');
+app.get('/', function(request, response) {
+  // DONE! TODO: Using the response object, send the index.html file back to the user
   response.sendFile('/public/index.html', {root: '.'});
 });
 
-// TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+// DONE! TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+app.get('/new', function(request, response) {
+  response.sendFile('/public/new.html', {root: '.'});
+});
+
+// STRETCH GOOOOOOAL: send back a 404 error
+app.get('*', function(request, response) {
+  response.status(404).end('404 error');
+});
 
 app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+  // DONE! TODO: Log to the console a message that lets you know which port your server has started on
+  console.log(`Running on port ${PORT}`);
 });
